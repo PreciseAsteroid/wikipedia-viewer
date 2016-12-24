55b2153c76fe7b5a89268f34c72f659120469adc
Amir <baruch.amir@gmail.com>
2016-12-24 09:05:31 +0200

    adding surge support

--- src/components/QueryPane.js
+++ src/components/QueryPane.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {withRouter} from 'react-router';
-import Helper from '../utils/wikiHelpers.js'
+import Helper from '../utils/wikiHelpers.js';
+import reactLogo from '../../public/react.svg';
+import reactRouterLogo from '../../public/reactrouter.svg';
 
 // TODO: seperate Quary pane to container and component
 const QueryPane = withRouter(
@@ -49,6 +51,14 @@ const QueryPane = withRouter(
               <input type="text" className="queryInput" onKeyUp={this.type} placeholder='search' />
           </form>
           <button style={linkStyle} onClick={()=>{this.handleRand()}} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} className="randButton">Random wiki</button>
+          <div>
+            <a href="https://facebook.github.io/react/">
+              <img alt="react" src={reactLogo} width="75" height="75" />
+            </a>
+            <a href="https://github.com/ReactTraining/react-router">
+              <img alt="react" src={reactRouterLogo} width="75" height="75" />
+            </a>
+          </div>
         </div>
       );
     }
